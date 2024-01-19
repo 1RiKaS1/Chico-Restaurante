50c94737f7808f3a975fdbbe0c4eb840f0243d49
Ricardo Henrique Silva Rodrigues <135380624+1RiKaS1@users.noreply.github.com>
2024-01-19 10:18:38 -0300

    Revert "Merge branch 'main' of https://github.com/1RiKaS1/Chico-Restaurante"
    This reverts commit e2767f5170d08e0e9706f0007ba3dcc35d1d0adf, reversing
    changes made to 6ec911ffec752da8286c538cbd95a1bcd90a1efa.

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-
-app.listen(5500, () => {
-    console.log("Server running on port 5500");
-    console.log("Access: http://localhost:5500")
-});
--- backend/services/hash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-// Criptografar a senha
-export const createPassowrdHash = async (password) => bcrypt.hash(password, 8);
-
-// Comparar a senha para login
-export const checkPassword = async (password, hash) => 
-                        bcrypt.compare(password, hash);
