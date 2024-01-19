0f0afacc0e023e6f03d8eadc214d81c5bdb87842
matheusdealcantara <dealcantaramatheus1@gmail.com>
2024-01-18 22:28:22 -0300

    add api files

---
+++ backend/database/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";    
+import config from "../config/database.js";
+
+class Database {
+    constructor(){
+        this.mongo();
+    }
+
+    mongo(){
+        this.connection = mongoose.connect(
+            config.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
+}
+
+export default new Database();
